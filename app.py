9b6d24379963e14a2662b77859e40aedefbb1141
PrajnyaSatish <prajnya.12@gmail.com>
2019-05-03 22:55:11 -0600

    Mofied webpage

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ from flask import render_template, redirect
 
 app = Flask(__name__)
 
+top_topics = {'bjp':{'sentiment': 0.02, 'length': 320}, 'loksabha': {'sentiment': 0.42, 'length': 555}}
+total_docs = 0
+news_headlines = {'headline1', 'headline2'}
+for k,v in top_topics.items():
+	total_docs += v['length']
 
 @app.route("/")
 def template_test():
-    return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+    return render_template('index.html', topics=top_topics, topic_docs=total_docs, headlines=list(news_headlines))
 
 
 if __name__ == '__main__':
