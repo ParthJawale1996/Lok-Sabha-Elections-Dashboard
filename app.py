16c0b8659f873b70c442df2f8be367c8aaf9f2cd
PrajnyaSatish <prajnya.12@gmail.com>
2019-05-05 14:10:13 -0600

    Changes to Vis

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask, Response
 from flask import render_template, redirect
 
+import json
+
 from redis_script import *
 
 
@@ -15,9 +17,15 @@ top_topics = r.get_redis('top_topics')
 names_and_sentiments = r.get_redis('names_and_sentiments')
 headlines = r.get_redis('headlines')
 
-print(top_topics)
-print(names_and_sentiments)
-print(headlines)
+with open('Politicians.json') as json_file:  
+    people_details = json.load(json_file)
+
+for name, dets in names_and_sentiments.items():
+	for p_name in people_details['politicians']:
+		if p_name['name']==name:
+			names_and_sentiments[name].update({'affiliation':p_name['affiliation'], 'state': p_name['state']})
+			print(names_and_sentiments[name])
+
 
 total_docs = 0
 news_headlines = {'headline1', 'headline2'}
@@ -26,7 +34,7 @@ for k,v in top_topics.items():
 
 @app.route("/")
 def template_test():
-    return render_template('index.html', topics=top_topics, topic_docs=total_docs, headlines=list(news_headlines))
+    return render_template('index.html', topics=top_topics, topic_docs=total_docs, headlines=headlines, politicians=names_and_sentiments)
 
 
 if __name__ == '__main__':
